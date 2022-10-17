c7b85103f0b80d640709c089944176d4c2fa157d
Lucian Green <luciangreen@lucianacademy.com>
2022-10-18 09:32:02 +1100

    Update truncate.pl

--- truncate.pl
+++ truncate.pl
@@ -11,14 +11,14 @@
 
 % given file with number of As required and gl file length, gives div and mod values for t2b
 
-truncate1(File1,Words1,File2) :-
+truncate1(Type,File1,Words1,File2) :-
 		
 		
 SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!'0123456789",
 
-	(true->
+	(Type=file->
 	(phrase_from_file_s(string(String2), File1));
-	_String1=String2),
+	(Type=string,File1=String2)),
 	split_string(String2,SepandPad,SepandPad,String3),
 	%writeln(String3),
 	length(String3,Words2),
@@ -34,6 +34,11 @@ SepandPad="&#@~%`$?-+*^,()|.:;=_/[]<>{}\n\r\s\t\\\"!'0123456789",
 	%term_to_atom(Item2,String4a),
 	%string_atom(String5,String4a),
 
+	(Type=file->
+
 	(open_s(File2,write,Stream3),
 	write(Stream3,String5),
-	close(Stream3)),!.
+	close(Stream3));
+	
+	(Type=string,File2=String5))
+	,!.
