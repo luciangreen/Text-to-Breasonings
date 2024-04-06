931641e1fe89b94dbde68a1027db14c852de198f
Lucian Green <luciangreen@lucianacademy.com>
2024-04-06 22:00:53 +1100

    Update meditatorsanddoctors.pl

--- meditatorsanddoctors.pl
+++ meditatorsanddoctors.pl
@@ -4,14 +4,16 @@
 
 meditators([
 %%/**
-[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal], %% 
-[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal] %% 
+% note if following invited to sim when in 5689 with texttobr2_1(*number of new medits).
+[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal], %% sim/not in sim
+[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal] %% sim/not in sim
 ]).
 
 meditators2([
 %%/**
-[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal], %% 
-[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal] %% 
+% note if following invited to sim when in 5689 with texttobr2_1(*number of new medits).
+[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal], %% sim/not in sim
+[first,last,dobd,dobm,doby,learntd,learntm,learnty,1,0,16,immortal] %% sim/not in sim
 ]).
 
 %% empty, because meditators is now based on 50 As
