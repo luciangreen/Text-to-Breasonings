90fd8ac78113e44907ea5e35d044eb7121cd7742
Lucian Green <luciangreen@lucianacademy.com>
2024-09-16 09:46:18 +1000

    Sped up anti_moral_end_meditators and text_to_breasonings

--- anti_moral_end_meditators.pl
+++ anti_moral_end_meditators.pl
@@ -1,8 +1,9 @@
 :-include('meditatorsanddoctors.pl').
 :-include('text_to_breasonings.pl').
 main:-catch((meditators(A),meditators2(B),length(A,AL),length(B,BL),CL is AL+BL,
-texttobr2(u,"16k_args.txt",u,u,[auto,on]),
-texttobr2(u,"16k_algs.txt",u,u,[auto,on]),
+%texttobr2(u,"16k_args.txt",u,u,[auto,on]),
+%texttobr2(u,"16k_algs.txt",u,u,[auto,on]),
+shell1_s("./bc12.sh"),
 H is 3,texttobr2_1(H), % 3 for a return time travel hop
 texttobr2_1(CL)),Err,handle_error(Err)),halt.
 
