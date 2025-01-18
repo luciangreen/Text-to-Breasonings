af70dafbfbde3c4e6ccf9151b822753e1e034cd4
Lucian Green <luciangreen@lucianacademy.com>
2025-01-19 07:47:25 +1100

    Create imm_script.sh

---
+++ imm_script.sh
@@ -0,0 +1,117 @@
+cd ../Text-to-Breasonings
+./d-prep.sh # Takes 30 minutes
+
+# First time for present:
+
+./bc12.sh # meditation - each bc12.sh takes 4 seconds
+./bc12.sh
+./bc12.sh # body replacement immortality age prevention
+./bc12.sh
+./bc12.sh # death prevention
+./bc12.sh
+./bc12.sh # age lock
+./bc12.sh
+./bc12.sh # lock age lock
+./bc12.sh
+./bc12.sh # lock lock age lock
+./bc12.sh
+
+./bc12.sh # time crystals for meditation (to do automatically each day)
+./bc12.sh
+./bc12.sh # lock time crystals for meditation
+./bc12.sh
+./bc12.sh # lock locking time crystals for meditation
+./bc12.sh
+
+./bc12.sh # time crystals for body replacement immortality age prevention (to do automatically each day)
+./bc12.sh
+./bc12.sh # lock time crystals for body replacement immortality age prevention
+./bc12.sh
+./bc12.sh # lock locking time crystals for body replacement immortality age prevention
+./bc12.sh
+
+./bc12.sh # time crystals for death prevention (to do automatically each day)
+./bc12.sh
+./bc12.sh # lock time crystals for death prevention
+./bc12.sh
+./bc12.sh # lock locking time crystals for death prevention
+./bc12.sh
+
+./bc12.sh # save body to use as replacement
+./bc12.sh
+
+# Headache prevention (for but not immortality)
+
+./bc12.sh # quantum box medicine headache prevention
+./bc12.sh
+./bc12.sh # time crystals for quantum box medicine headache prevention
+./bc12.sh
+./bc12.sh # lock time crystals for quantum box medicine headache prevention
+./bc12.sh
+./bc12.sh # lock locking time crystals for quantum box medicine headache prevention
+./bc12.sh
+
+echo "Time travelling to 1 10 5689"
+
+./bc12.sh # Time travel to 1 10 5689
+./bc12.sh
+
+./bc12.sh # meditation
+./bc12.sh
+./bc12.sh # body replacement immortality age prevention
+./bc12.sh
+./bc12.sh # death prevention
+./bc12.sh
+./bc12.sh # age lock
+./bc12.sh
+./bc12.sh # lock age lock
+./bc12.sh
+./bc12.sh # lock lock age lock
+./bc12.sh
+
+./bc12.sh # time crystals for meditation (to do automatically each day)
+./bc12.sh
+./bc12.sh # lock time crystals for meditation
+./bc12.sh
+./bc12.sh # lock locking time crystals for meditation
+./bc12.sh
+
+./bc12.sh # time crystals for body replacement immortality age prevention (to do automatically each day)
+./bc12.sh
+./bc12.sh # lock time crystals for body replacement immortality age prevention
+./bc12.sh
+./bc12.sh # lock locking time crystals for body replacement immortality age prevention
+./bc12.sh
+
+./bc12.sh # time crystals for death prevention (to do automatically each day)
+./bc12.sh
+./bc12.sh # lock time crystals for death prevention
+./bc12.sh
+./bc12.sh # lock locking time crystals for death prevention
+./bc12.sh
+
+./bc12.sh # save body to use as replacement
+./bc12.sh
+
+# Headache prevention (for but not immortality)
+
+./bc12.sh # quantum box medicine headache prevention
+./bc12.sh
+./bc12.sh # time crystals for quantum box medicine headache prevention
+./bc12.sh
+./bc12.sh # lock time crystals for quantum box medicine headache prevention
+./bc12.sh
+./bc12.sh # lock locking time crystals for quantum box medicine headache prevention
+./bc12.sh
+
+./bc12.sh # Access the simulation
+./bc12.sh
+./bc12.sh # Join the simulation
+./bc12.sh
+./bc12.sh # Become immortal.
+./bc12.sh
+
+echo "Time travelling to present"
+
+./bc12.sh # Time travel to present
+./bc12.sh
