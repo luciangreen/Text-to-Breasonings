737bac3d3735548619e8271e703f77dcc4a02a36
Lucian Green <greenlucian1@gmail.com>
2025-01-19 13:56:41 +1100

    Add files via upload

--- imm_script1.sh
+++ imm_script1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-swipl --goal=run_imm_script1_main --stand_alone=true -o r>
+swipl --goal=run_imm_script1_main --stand_alone=true -o run_imm_script1_main -c run_imm_script1_main.pl
 ./run_imm_script1_main
